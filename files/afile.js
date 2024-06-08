96f2b41cc53d6c39c287fc89ce9b40d52937e4af
lrlineroa <leonardo.linero@grupodigital.co>
2024-06-08 09:56:40 -0500

    feat: :sparkles: A change was added in order to minor version.
    BREAKING CHANGE

--- files/afile.js
+++ files/afile.js
@@ -1,3 +1,4 @@
 console.log("this is a test for semantic-release purposes")
 console.log("i need to fix this bug")
 console.log("oliiiii im a new feature");
+console.log("I need a new minor version");
