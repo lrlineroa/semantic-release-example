88d9c23918baa1aaa2dc798d59b2a2b294c4e43c
lrlineroa <leonardo.linero@grupodigital.co>
2024-06-08 10:19:40 -0500

    feat: :sparkles: a really version

--- files/afile.js
+++ files/afile.js
@@ -4,5 +4,5 @@ console.log("oliiiii im a new feature");
 console.log("I need a new minor version");
 console.log("I need a really new minor version");
 console.log("I need a really really new minor version");
-console.log("I need a really really new minor version");
+console.log("I need a really really new minor version but this will be only a feat");
 
