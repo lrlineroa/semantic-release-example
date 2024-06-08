bad187ac1c8ad364ba45636aa2c3f20484910817
lrlineroa <leonardo.linero@grupodigital.co>
2024-06-08 10:01:15 -0500

    feat: :sparkles: I need really a new major o minor

--- files/afile.js
+++ files/afile.js
@@ -2,3 +2,4 @@ console.log("this is a test for semantic-release purposes")
 console.log("i need to fix this bug")
 console.log("oliiiii im a new feature");
 console.log("I need a new minor version");
+console.log("I need a really new minor version");
